b7dabde28b86d086c623ebac0708ef0e80494ce8
sebk-666 <seb.kaps@zoho.com>
2020-11-27 18:00:07 +0100

    Improved request handling.

--- de.sebastian-kaps.HALightsControl.js
+++ de.sebastian-kaps.HALightsControl.js
@@ -1,7 +1,7 @@
 /*!
 @title HALightsControl
 @author Sebastian Kaps
-@version 0.0
+@version 0.1
 */
 
 /*
@@ -25,24 +25,27 @@ var pageChanged = 0;
 function haService (domain, service, entityId) {
     request = new com.philips.HttpLibrary.HttpRequest();
     request.open("POST", "http://" + ha_host + ":" + ha_port + "/api/services/" + domain + "/" + service, true);
-    request.setRequestHeader("Authorization:", "Bearer " + ha_token);
-    request.setRequestHeader("Content-Type:", "application/json");
-	request.send('{"entity_id": "' +  entityId + '"}');
+    request.setRequestHeader("Authorization", "Bearer " + ha_token);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.setRequestHeader("Connection", "Close");
+    request.send('{"entity_id": "' +  entityId + '"}');
 }
 
 function haSetState(entityId, state) {
     request = new com.philips.HttpLibrary.HttpRequest();
     request.open("POST", "http://" + ha_host + ":" + ha_port + "/api/states/" + entityId, true);
-    request.setRequestHeader("Authorization:", "Bearer " + ha_token);
-    request.setRequestHeader("Content-Type:", "application/json");
+    request.setRequestHeader("Authorization", "Bearer " + ha_token);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.setRequestHeader("Connection", "Close");
 	request.send('{"state": "' +  state + '"}');
 }
 
 function haSetAttribute(entityId, attribute, value) {
     request = new com.philips.HttpLibrary.HttpRequest();
     request.open("POST", "http://" + ha_host + ":" + ha_port + "/api/states/" + entityId, true);
-    request.setRequestHeader("Authorization:", "Bearer " + ha_token);
-    request.setRequestHeader("Content-Type:", "application/json");
+    request.setRequestHeader("Authorization", "Bearer " + ha_token);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.setRequestHeader("Connection", "Close");
     request.send('{"state": "home", "attributes": {"' + attribute + '": ' + '"' + value + '"}}');
 }
 
@@ -56,8 +59,9 @@ function haLightChangeBrightness(entityId, delta_pct) {
     if (newbri  >= 0 && newbri <= 100) {
         request = new com.philips.HttpLibrary.HttpRequest();
         request.open("POST", "http://" + ha_host + ":" + ha_port + "/api/services/light/turn_on", true);
-        request.setRequestHeader("Authorization:", "Bearer " + ha_token);
-        request.setRequestHeader("Content-Type:", "application/json");
+        request.setRequestHeader("Authorization", "Bearer " + ha_token);
+        request.setRequestHeader("Content-Type", "application/json");
+        request.setRequestHeader("Connection", "Close");
         request.send('{"entity_id": "' +  entityId + '",'
                         + '"brightness_pct":' + '"' + newbri + '"'
                         + '}');
@@ -65,20 +69,33 @@ function haLightChangeBrightness(entityId, delta_pct) {
     }
 }
 
-var request = [];
+function haLightBrightnessStep(entityId, delta_pct) {
+    request = new com.philips.HttpLibrary.HttpRequest();
+    request.open("POST", "http://" + ha_host + ":" + ha_port + "/api/services/light/turn_on", true);
+    request.setRequestHeader("Authorization", "Bearer " + ha_token);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.setRequestHeader("Connection", "Close");
+    request.send('{"entity_id": "' +  entityId + '",'
+                    + '"brightness_step_pct":' + '"' + delta_pct + '"'
+                    + '}');
+}
+
+var stateRequest = {};
 function getState(entityId) {
     //System.setDebugMask(9);
-    request[entityId] = new com.philips.HttpLibrary.HttpRequest();
-    request[entityId].open("GET", "http://" + ha_host + ":" + ha_port + "/api/states/" + entityId, true);
-    request[entityId].setRequestHeader("Authorization:", "Bearer " + ha_token);
-    request[entityId].setRequestHeader("Content-Type:", "application/json");
+    //System.print("GetState: " + entityId);
+    stateRequest[entityId] = new com.philips.HttpLibrary.HttpRequest();
+    stateRequest[entityId].open("GET", "http://" + ha_host + ":" + ha_port + "/api/states/" + entityId, true);
+    stateRequest[entityId].setRequestHeader("Authorization", "Bearer " + ha_token);
+    stateRequest[entityId].setRequestHeader("Content-Type", "application/json");
+    stateRequest[entityId].setRequestHeader("Connection", "Close");
 
     var state;
-    request[entityId].onreadystatechange = function() {
-        if (request[entityId].readyState == 4) {
+    stateRequest[entityId].onreadystatechange = function() {
+        if (stateRequest[entityId].readyState == 4 && stateRequest[entityId].status == 200) {
+            //System.print("Response: " + entityId)
             regex = /"state": "(\w+)"/;
-            state = (request[entityId].responseText).match(regex)[1];
-            System.print(entityId);
+            state = (stateRequest[entityId].responseText).match(regex)[1];
             if (state == "on") {
                 CF.widget(entityId).setImage(IMG_ON);
                 CF.widget(entityId).label = "on";
@@ -86,41 +103,44 @@ function getState(entityId) {
                 CF.widget(entityId).setImage(IMG_OFF);
                 CF.widget(entityId).label = "off";
             }
+            delete stateRequest[entityId];
         }
     }
-    request[entityId].send();
+    stateRequest[entityId].send();
 };
 
 function haSetScene(roomName, scnName) {
     request = new com.philips.HttpLibrary.HttpRequest();
     request.open("POST", "http://" + ha_host + ":" + ha_port + "/api/services/hue/hue_activate_scene", true);
-    request.setRequestHeader("Authorization:", "Bearer " + ha_token);
-    request.setRequestHeader("Content-Type:", "application/json");
+    request.setRequestHeader("Authorization", "Bearer " + ha_token);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.setRequestHeader("Connection", "Close");
 	request.send('{"group_name": "' +  roomName + '", "scene_name": "' + scnName  + '"}');
 };
 
 // return the brightness value for a single lamp
 function haGetBrightness(entityId) {
-    request[entityId] = new com.philips.HttpLibrary.HttpRequest();
-    request[entityId].open("GET", "http://" + ha_host + ":" + ha_port + "/api/states/" + entityId, true);
-    request[entityId].setRequestHeader("Authorization:", "Bearer " + ha_token);
-    request[entityId].setRequestHeader("Content-Type:", "application/json");
+    stateRequest[entityId] = new com.philips.HttpLibrary.HttpRequest();
+    stateRequest[entityId].open("GET", "http://" + ha_host + ":" + ha_port + "/api/states/" + entityId, true);
+    stateRequest[entityId].setRequestHeader("Authorization", "Bearer " + ha_token);
+    stateRequest[entityId].setRequestHeader("Content-Type", "application/json");
 
     var state;
-    request[entityId].onreadystatechange = function() {
-        if (request[entityId].readyState == 4) {
+    stateRequest[entityId].onreadystatechange = function() {
+        if (stateRequest[entityId].readyState == 4 && stateRequest[entityId].status == 200) {
             // only turned on lamps have 'brightness' attribute, so let's check
             // the lamp's state before trying to read the bri value
             regex = /"state": "(\w+)"/;
-            state = (request[entityId].responseText).match(regex)[1];
+            state = (stateRequest[entityId].responseText).match(regex)[1];
             if (state == "on") {
                 regex = /"brightness": (\d+)/;
-                bri = (request[entityId].responseText).match(regex)[1];
+                bri = (stateRequest[entityId].responseText).match(regex)[1];
                 System.setGlobal('activelight_bri', Math.ceil(bri / 254 * 100));
             }
+            delete stateRequest[entityId];
         }
     }
-    request[entityId].send();
+    stateRequest[entityId].send();
 };
 
 /*
@@ -193,7 +213,10 @@ function lightButtonHelper(light) {
 function setupRotary() {
     CF.activity().onRotary = function(clicks) {
         if (activelight && CF.widget(activelight).label == "on") {
-            haLightChangeBrightness(activelight, 5 * clicks);
+           // haLightChangeBrightness(activelight, 5 * clicks); // pre HA 0.107.x
+
+           // this requires Home-Assistent Core >v0.107
+           haLightBrightnessStep(activelight, 5 * clicks);
         }
     }
 }
@@ -225,7 +248,13 @@ function updateLoop(lights) {
 
 // we need to have an exit condition for the scheduleAfter() function;
 // beware of onEntry as that's run after the page script;
-// this needs to be a counter to avoid setting multiple timers with double-taps
+// this needs to be a counter to avoid setting multiple timers with accidental double-taps
 (function(){
-    CF.page().onExit = function(){pageChanged++; };
+    CF.page().onExit = function(){
+                        pageChanged++;
+                        // invalidate all .onreadystatechange callbacks when exiting the page
+                        for (req in stateRequest) {
+                            delete stateRequest.req;
+                        }
+                    };
 }());
